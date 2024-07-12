c557094c9368819d0663fff05b79538532d97fce
Vivin Vinod <64364161+vivinvinod@users.noreply.github.com>
2024-07-12 16:26:42 +0200

    Update CreateDataSet.py to include calculation time for each fidelity

--- CreateDataSet.py
+++ CreateDataSet.py
@@ -1,10 +1,19 @@
 import numpy as np
 
-def create_CheMFiDataset(molname):
+def return_unitimes(mol):
+    times = np.zeros((5,10),dtype=float)
+    fids = np.asarray(['STO3G','321G','631G','SVP','TZVP'])
+    for i in range(5):
+        temp = np.loadtxt(f'propertyzips/uniformtime/{mol}/{mol}_{fids[i]}_unitimes.dat')
+        #mins s ms
+        times[i] = temp[:,0]*60 + temp[:,1] + temp[:,2]/1000.0
+    return times.mean(axis=1)
+
+def create_MF24Dataset(molname):
     fids = np.asarray(['STO3G','321G','631G','SVP','TZVP'])
     sampling = np.arange(0,120000)[::int(120000/15000)]
     
-    ws22_raw = np.load(f'ws22_{molname}.npz',allow_pickle=True) #raw file from WS22 database
+    ws22_raw = np.load(f'ws22raw/ws22_{molname}.npz',allow_pickle=True)
     coords = ws22_raw['R'][sampling]
     Zs = ws22_raw['Z']
     confs = ws22_raw['CONF'][:,0][sampling]
@@ -17,7 +26,7 @@ def create_CheMFiDataset(molname):
     DPn = np.zeros((15000,5,3),dtype=float)
     RCo = np.zeros((15000,5,3),dtype=float)
     DPRo = np.zeros((15000,5,3),dtype=float)
-    
+    times = return_unitimes(molname)
     for i in range(5):
         SCF[:,i] = np.loadtxt(f'propertyzips/{molname}/{fids[i]}_SCF.dat') #*27.211407953 #convert eh to ev
         EV[:,i,:] = np.loadtxt(f'propertyzips/{molname}/{fids[i]}_EV.dat') #*0.000124 #convert cm-1 to ev
@@ -35,12 +44,13 @@ def create_CheMFiDataset(molname):
              SCF=SCF, 
              EV=EV, TrDP=TrDP, fosc=fosc,
              DPe=DPe, DPn=DPn, 
-             RCo=RCo, DPRo=DPRo
+             RCo=RCo, DPRo=DPRo,
+             t=times
             )
     
 def main():
     mol_list = np.asarray(['urea','acrolein','alanine','sma','nitrophenol','urocanic','dmabn','thymine','o-hbdi'])
     for m in mol_list:
-        create_CheMFiDataset(molname=m)
+        create_MF24Dataset(molname=m)
 
 main()
