affdee95ce5e0261daaff38713bc429b1498d0c8
Vivin Vinod <64364161+vivinvinod@users.noreply.github.com>
2024-07-23 10:14:29 +0200

    Update SpecialStudy.py for time vs MAE plot data
    Saves indices of training samples used so they can be used to access corresponding time data in the learning curves.

--- MFML/SpecialStudy.py
+++ MFML/SpecialStudy.py
@@ -9,7 +9,7 @@ def prep_data():
     molnames = ['urea','acrolein','alanine','sma','nitrophenol','urocanic','dmabn','thymine','o-hbdi']
     idx = np.arange(0,15000)
     idx = shuffle(idx,random_state=42)
-    idx=idx[:1500] 
+    idx=idx[:1500]
     
     X=np.zeros((13500,6438),dtype=float) #largest SLATM is for o-hbdi with 6438 features. Rest will be padded to this size.
     y_all = np.zeros((13500,5),dtype=float)
@@ -21,14 +21,15 @@ def prep_data():
     for i,m in tqdm(enumerate(molnames),desc='building composite dataset of uncentered SCF'):
         names = np.full(1500,i)
         idx_names[start:end] = np.copy(names)
-        temp_X = np.load(f'Reps/{m}_SLATM.npy') #change as needed
+        temp_X = np.load(f'Reps/{m}_SLATM.npy')
         X[start:end,:temp_X.shape[-1]] = temp_X[idx,:]
-        temp_data=np.load(f'../dataset/CheMFi_{m}.npz')['SCF'] #change as needed
+        temp_data=np.load(f'../dataset/CheMFi_{m}.npz')['SCF']
         y_all[start:end,:] = temp_data[idx,:]
         
         #increment for next molecule
         start+= 1500
         end += 1500
+    
     y_new = np.zeros((5),dtype=object)
     
     X,idx_names = shuffle(X,idx_names,random_state=42)
@@ -39,12 +40,10 @@ def prep_data():
         y_train[i] = y_new[i][:11000]
     
     X_train = X[:11000,:]
-    #y_train = y_new[:11000,:]
     X_val = X[11000:11500,:]
     y_val = y_new[-1][11000:11500]
     X_test = X[11500:,:]
     y_test = y_new[-1][11500:]
-    
     
     return X_train, X_val, X_test, y_train, y_val, y_test, idx_names
     
@@ -82,7 +81,7 @@ def SF_learning_curve(X_train:np.ndarray, X_test:np.ndarray, y_train:np.ndarray,
     for n in tqdm(range(navg),desc='average LC generation'):
         maes = []
         X_train,y_train = shuffle(X_train, y_train, random_state=42)
-        for i in tqdm(range(1,10),desc='learning curve routine', leave=False):
+        for i in range(1,10):
             temp, preds = KRR(X_train[:2**i],X_test,y_train[:2**i],y_test,sigma=sigma,reg=reg,k_type=k_type)
             maes.append(temp)
         full_maes += np.asarray(maes)
@@ -97,7 +96,7 @@ def LC_routine(y_trains:np.ndarray, indexes:np.ndarray, X_train:np.ndarray, X_te
     MAEs_OLS = np.zeros((9),dtype=float) #for OLS MFML
     MAEs_def = np.zeros((9),dtype=float) # for default MFML
     
-    for i in range(navg):
+    for i in tqdm(range(navg),desc='avg run',leave=False):
         mae_ntr_OLS = []
         mae_ntr_def = []
         for j in range(1,10):
@@ -140,7 +139,7 @@ def varying_baselines(X_train, X_val, X_test, y_train, y_val, y_test, ker:str='l
     
     #run single fidelity KRR for given molecule
     sf_maes,sf_preds = SF_learning_curve(X_train=X_train, X_test=X_test, 
-                                  y_train=y_train[-1], y_test=y_test, k_type=ker,
+                                  y_train=y_train[0], y_test=y_test, k_type=ker,
                                   sigma=200.0, reg=1e-10,
                                   navg=navg) 
     
@@ -164,6 +163,7 @@ def varying_baselines(X_train, X_val, X_test, y_train, y_val, y_test, ker:str='l
     np.save('outputs/specialcase/ols_mae.npy',maeols)
     
 def main():
+    #TZVP first in y_trains
     X_train, X_val, X_test, y_train, y_val, y_test, mol_IDs = prep_data()
     varying_baselines(X_train, X_val, X_test, y_train, y_val, y_test, sig=200.0, reg=1e-10, navg=10)
 
@@ -202,5 +202,45 @@ def predict_main():
     np.save('outputs/specialcase/def_preds.npy',defpreds)
     np.save('outputs/specialcase/ols_preds.npy',olspreds)
 
+def save_inds_for_time():
+    X_train, X_val, X_test, y_train, y_val, y_test, mol_IDs = prep_data()
+    indexes = np.zeros((5),dtype=object)
+    for i in range(5):
+        indexes[i] = np.vstack([np.arange(0,11000),np.arange(0,11000)]).T
+    
+    defmaes = np.zeros((4,9),dtype=float)
+    olsmaes = np.zeros((4,9),dtype=float)
+    modelinds = np.zeros((4),dtype=object)
+    
+    for fb in range(4):
+        for n in range(1,10):
+            n_trains = np.asarray([2**(n+4),2**(n+3),2**(n+2),2**(n+1),2**n])[fb:]
+            ###TRAINING######
+            model = MFML(reg=1e-10, kernel='laplacian', 
+                         order=1, metric='l2', 
+                         sigma=200.0, p_bar=False)
+
+            model.train(X_train_parent=X_train, 
+                        y_trains=y_train[fb:], indexes=indexes[fb:], 
+                        shuffle=True, n_trains=n_trains, seed=42)
+            #default MFML
+            _ = model.predict(X_test = X_test, y_test = y_test, 
+                                    X_val = X_val, y_val = y_val, 
+                                    optimiser='default')
+            defmaes[fb,n-1] = np.copy(model.mae)
+            #OLS o-MFML
+            _ = model.predict(X_test = X_test, y_test = y_test, 
+                                     X_val = X_val, y_val = y_val, 
+                                     optimiser='OLS', copy_X= True, 
+                                     fit_intercept= False)
+            olsmaes[fb,n-1]=np.copy(model.mae)
+        modelinds[fb] = np.copy(model.indexes)
+    
+    np.save('outputs/specialcase/index_def_maes.npy',defmaes)
+    np.save('outputs/specialcase/index_ols_maes.npy',olsmaes)
+    np.save('outputs/specialcase/index_index.npy',modelinds)
+    
+
 main()
 predict_main()
+save_inds_for_time()
